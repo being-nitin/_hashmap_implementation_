079e17c2677ed46282dea371ed64c1a2ae85fad0
Nitin <nsinghboss02@gmail.com>
2022-03-09 08:23:00 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,21 @@
+package com.Recursion;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/* ..Hashmap..
+	 Map stores the key value pairs.
+	 */
+        // In this order is not maintained.
+
+    // key,value.
+
+    Map<String,Integer> map = new HashMap<>();
+    map.put("one",1);
+    map.put("two",1);
+    map.put("three",1);
+    map.put("four",1);
+        System.out.println(map);
+    }
+
+}
